a9dceb1d261e52e6238b37741596e67a0f0d885b
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2021-12-16 15:36:10 +0300

    change style

--- src/MyProjects/MyProjects.tsx
+++ src/MyProjects/MyProjects.tsx
@@ -7,7 +7,7 @@ import {MyProject} from "./MyProject/MyProject";
 export const MyProjects = () => {
     return <div className={style.myProjectBlock}>
         <div className={`${commonStyle.container} ${style.myProjectContainer}`}>
-            <h2 className={style.title}>My projects</h2>
+            <h2 className={style.title}>My Projects</h2>
             <div className={style.myProjects}>
                 <MyProject title={'Name of project'}
                            description={'dipisicing Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt.'}/>

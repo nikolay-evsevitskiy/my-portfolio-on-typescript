79a9a9b747904a4a347bc199de82cf685e733c4e
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-02-25 16:13:16 +0300

    create icons on footer

--- src/Common/IconButton/IconButton.tsx
+++ src/Common/IconButton/IconButton.tsx
@@ -3,11 +3,16 @@ import style from './IconButton.module.scss'
 
 type IconButtonPropsType = {
     description: string
+    imgAddress: string
     urlAddress: string
+
 }
 
-export const IconButton: React.FC<IconButtonPropsType>= ({description,urlAddress}) => {
-    return <div className={style.IconButtonBlock}>
-        <img src={urlAddress} alt={description}/>
+export const IconButton: React.FC<IconButtonPropsType> = ({description, imgAddress, urlAddress}) => {
+    return <div>
+        <a href={urlAddress}>
+            <img src={imgAddress} alt={description} title={description} className={style.IconButtonBlock}/>
+        </a>
+
     </div>
 }
--- src/FooterBlock/Footer.tsx
+++ src/FooterBlock/Footer.tsx
@@ -3,23 +3,28 @@ import style from './FooterWork.module.scss'
 import {IconButton} from "../Common/IconButton/IconButton";
 import commonStyle from "./../Common/Styles/Styles.module.css"
 import {Title} from "../Common/Components/Title/Title";
-import reactIconImg from "../assets/images/atom.png";
+import instagramIconImg from "../assets/images/instagram.png";
+import linkedInIconImg from "../assets/images/linkedin.png";
+import gitHubIconImg from "../assets/images/github (1).png";
+import codeWarsIconImg from "../assets/images/codewars.svg";
 
 
 export const Footer = () => {
-    const instagramIcon = {
-        backgroundImage: `url(${reactIconImg})`
-    }
+    const instagramAddress = 'https://www.instagram.com/nikolai_evsevitski'
+    const linkedInAddress = 'https://www.linkedin.com/in/nikolay-evsevitskiy'
+    const gitHubAddress = 'https://github.com/nikolay-evsevitskiy'
+    const codeWarsAddress = 'https://www.codewars.com/users/nikola007'
+
     return <div className={style.footerBlock}>
         <div className={`${commonStyle.container} ${style.footerContainer}`}>
-            <Title text={'Nikolay Evsevitsky'} />
+            <Title text={'My pages'}/>
             <div className={style.iconBlock}>
-                <IconButton description={'description'} urlAddress={''}/>
-                <IconButton description={'description'} urlAddress={''}/>
-                <IconButton description={'description'} urlAddress={''}/>
-                <IconButton description={'description'} urlAddress={''}/>
+                <IconButton description={'Instagram'} imgAddress={instagramIconImg} urlAddress={instagramAddress}/>
+                <IconButton description={'Linked In'} imgAddress={linkedInIconImg}  urlAddress={linkedInAddress}/>
+                <IconButton description={'Git Hub'} imgAddress={gitHubIconImg}  urlAddress={gitHubAddress}/>
+                <IconButton description={'Code wars'} imgAddress={codeWarsIconImg}  urlAddress={codeWarsAddress}/>
             </div>
-            <h3 className={style.copyBlock}>©2021 All rights reserved</h3>
+            <h3 className={style.copyBlock}>©2022 All rights reserved</h3>
         </div>
     </div>
 }

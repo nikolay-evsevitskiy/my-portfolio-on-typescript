138e3e50cd4a203e1a666ae1eab624c086a10a50
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-02-25 16:23:59 +0300

    added url addresses on buttons

--- src/MyProjects/MyProject/MyProject.tsx
+++ src/MyProjects/MyProject/MyProject.tsx
@@ -6,13 +6,14 @@ type MyProjectPropsType = {
     title: string
     description: string
     styleImage?: {}
+    urlAddress: string
 }
 
 
-export const MyProject: React.FC<MyProjectPropsType> = ({title, description, styleImage}) => {
+export const MyProject: React.FC<MyProjectPropsType> = ({title, description, styleImage, urlAddress}) => {
     return <div className={style.MyProjectBlock}>
         <div className={style.image} style={styleImage}>
-            <Button title={"Look"} address={"#"}/>
+            <Button title={"Look"} address={urlAddress}/>
         </div>
         <h3 className={style.title}>{title}</h3>
         <div className={style.description}>{description}</div>
--- src/MyProjects/MyProjects.tsx
+++ src/MyProjects/MyProjects.tsx
@@ -8,7 +8,6 @@ import socialNetImage from "../assets/images/SocialNetWork1.png";
 import counterImage from "../assets/images/Counter.png";
 
 
-
 export const MyProjects = () => {
     const todolist = {
         backgroundImage: `url(${todolistImage})`
@@ -19,6 +18,9 @@ export const MyProjects = () => {
     const counterImg = {
         backgroundImage: `url(${counterImage})`
     }
+    const todolistUrlAddress = "https://nikolay-evsevitskiy.github.io/todolist/"
+    const counterUrlAddress = "https://nikolay-evsevitskiy.github.io/Counter/"
+    const socialNetworkUrlAddress = "https://nikolay-evsevitskiy.github.io/todolist/"
 
     return <div className={style.myProjectBlock}>
         <div className={`${commonStyle.container} ${style.myProjectContainer}`}>
@@ -26,12 +28,15 @@ export const MyProjects = () => {
             <div className={style.myProjects}>
                 <MyProject title={"Todolist"}
                            styleImage={todolist}
+                           urlAddress={todolistUrlAddress}
                            description={"In this application, you can create various task lists, mark them as completed, sort and edit them"}/>
                 <MyProject title={'Social network'}
                            styleImage={socialNet}
+                           urlAddress={socialNetworkUrlAddress}
                            description={"This is a social network application, implemented in React, Redux, TypeScript"}/>
                 <MyProject title={"Counter"}
                            styleImage={counterImg}
+                           urlAddress={counterUrlAddress}
                            description={"This is a simple counter application"}/>
 
             </div>

ca31942f8cc4fefb1ad9fa327b0a0197179ab147
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-03-01 20:19:44 +0300

    fixed header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,7 +3,10 @@ import style from "./Header.module.scss"
 import {Nav} from "../Nav/Nav";
 
 export const Header = () => {
-    return <div className={style.header}>
-        <Nav/>
-    </div>
+    return <>
+        <div className={style.header}/>
+        <div className={style.headerFixed}>
+            <Nav/>
+        </div>
+    </>
 }

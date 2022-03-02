b6e98b6f92de08a0e6a6bfa525603495c7df8256
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-03-02 23:59:30 +0300

    include graphics

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import style from "./Main.module.scss"
 import commonStyle from "../Common/Styles/Styles.module.css"
 import photoImg from "../assets/images/IMG_9909.jpg"
-
-
-
-
+import Particles from "react-tsparticles";
 
 
 export const Main = () => {
@@ -13,7 +10,86 @@ export const Main = () => {
         backgroundImage: `url(${photoImg})`
     }
     return <div className={style.mainBlock} id={"mainId"}>
-
+        <Particles
+            className={style.particles}
+            options={{
+                background: {
+                    color: {
+                        value: "#111319",
+                    },
+                },
+                fpsLimit: 100,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: false,
+                            mode: "repulse",
+                        },
+                        resize: false,
+                    },
+                    modes: {
+                        bubble: {
+                            distance: 500,
+                            duration: 3,
+                            opacity: 0.8,
+                            size: 40,
+                        },
+                        push: {
+                            quantity: 8,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#fcac45",
+                    },
+                    links: {
+                        color: "#fcac45",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outMode: "bounce",
+                        random: true,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        random: true,
+                        value: 5,
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
         <div className={`${commonStyle.container} ${style.mainContainer}`}>
             <div className={style.text}>
                 <span>Hi There</span>

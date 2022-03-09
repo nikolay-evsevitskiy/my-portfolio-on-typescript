f4745d3ec13f299c125b6eff65ba760ba6c463e4
Nikolay <83282653+nikolay-evsevitskiy@users.noreply.github.com>
2022-03-09 15:11:39 +0300

    refactoring in app style component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import styles from "./App.module.scss"
 
 function App() {
   return (
-      <div className={styles.app}>
+      <div className={styles.App}>
         <Header/>
         <Main/>
         <Skills/>
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -13,88 +13,8 @@ export const Main = () => {
         backgroundImage: `url(${photoImg})`
     }
     return <div className={style.mainBlock} id={"mainId"}>
-
         <div className={`${commonStyle.container} ${style.mainContainer}`}>
-            <Particles
-                className={style.particles}
-                options={{
-                    background: {
-                        color: {
-                            value: "#111319",
-                        },
-                    },
-                    fpsLimit: 100,
-                    interactivity: {
-                        events: {
-                            onClick: {
-                                enable: false,
-                                mode: "push",
-                            },
-                            onHover: {
-                                enable: false,
-                                mode: "repulse",
-                            },
-                            resize: false,
-                        },
-                        modes: {
-                            bubble: {
-                                distance: 500,
-                                duration: 3,
-                                opacity: 0.8,
-                                size: 40,
-                            },
-                            push: {
-                                quantity: 8,
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4,
-                            },
-                        },
-                    },
-                    particles: {
-                        color: {
-                            value: "#fcac45",
-                        },
-                        links: {
-                            color: "#fcac45",
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1,
-                        },
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            direction: "none",
-                            enable: true,
-                            outMode: "bounce",
-                            random: true,
-                            speed: 1,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                area: 800,
-                            },
-                            value: 80,
-                        },
-                        opacity: {
-                            value: 0.5,
-                        },
-                        shape: {
-                            type: "circle",
-                        },
-                        size: {
-                            random: true,
-                            value: 5,
-                        },
-                    },
-                    detectRetina: true,
-                }}
-            />
+
             <div className={style.text}>
                 <span>Hi There</span>
                 <h1>I am <span className={style.fullName}>Nikolay Evsevitsky</span></h1>
